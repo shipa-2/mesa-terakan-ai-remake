30366500e4293629056fe488a9a39c56ae878d6f
Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>
2023-06-13 21:33:04 +0300

    r600: Copy draw-related packet definitions to evergreend.h
    Signed-off-by: Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>

--- src/gallium/drivers/r600/evergreend.h
+++ src/gallium/drivers/r600/evergreend.h
@@ -49,6 +49,9 @@
 #define R600_TEXEL_PITCH_ALIGNMENT_MASK        0x7
 
 #define PKT3_NOP                               0x10
+#define EG_PKT3_SET_BASE                       0x11
+#define     EG_DRAW_INDEX_INDIRECT_PATCH_TABLE_BASE 1 /* DX11 Draw_Index_Indirect Patch Table Base */
+#define EG_PKT3_INDEX_BUFFER_SIZE              0x13
 #define PKT3_DEALLOC_STATE                     0x14
 #define PKT3_DISPATCH_DIRECT                   0x15
 #define PKT3_DISPATCH_INDIRECT                 0x16
@@ -57,10 +60,19 @@
 #define PKT3_REG_RMW                           0x21
 #define PKT3_COND_EXEC                         0x22
 #define PKT3_PRED_EXEC                         0x23
+#define EG_PKT3_DRAW_INDIRECT                  0x24
+#define EG_PKT3_DRAW_INDEX_INDIRECT            0x25
+#define EG_PKT3_INDEX_BASE                     0x26
 #define PKT3_DRAW_INDEX_2                      0x27
 #define PKT3_CONTEXT_CONTROL                   0x28
 #define PKT3_DRAW_INDEX_IMMD_BE                0x29
 #define PKT3_INDEX_TYPE                        0x2A
+#define     VGT_INDEX_16                   0
+#define     VGT_INDEX_32                   1
+#define         VGT_DMA_SWAP_NONE          (0 << 2)
+#define         VGT_DMA_SWAP_16_BIT        (1 << 2)
+#define         VGT_DMA_SWAP_32_BIT        (2 << 2)
+#define         VGT_DMA_SWAP_WORD          (3 << 2)
 #define PKT3_DRAW_INDEX                        0x2B
 #define PKT3_DRAW_INDEX_AUTO                   0x2D
 #define PKT3_DRAW_INDEX_IMMD                   0x2E
