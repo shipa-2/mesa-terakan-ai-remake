99927570a8079f05fc2224f6189743d11be224e7
Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>
2023-08-20 00:31:34 +0300

    terakan: Pipeline layout
    Signed-off-by: Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>

---
+++ src/amd/terascale/vulkan/terakan_pipeline_layout.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2023 Vitaliy Triang3l Kuzmin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef TERAKAN_PIPELINE_LAYOUT_H
+#define TERAKAN_PIPELINE_LAYOUT_H
+
+#include "compiler/shader_enums.h"
+#include "vk_pipeline_layout.h"
+
+#include <stdint.h>
+
+struct terakan_pipeline_layout_set {
+   uint8_t first_shader_resources[MESA_SHADER_STAGES];
+   uint8_t first_shader_samplers[MESA_SHADER_STAGES];
+};
+
+struct terakan_pipeline_layout {
+   struct vk_pipeline_layout vk;
+
+   struct terakan_pipeline_layout_set * sets;
+
+   uint32_t shader_non_immutable_samplers[MESA_SHADER_STAGES];
+   uint32_t shader_immutable_samplers_unnormalized_coordinates[MESA_SHADER_STAGES];
+
+   uint32_t shader_push_constant_extents_bytes[MESA_SHADER_STAGES];
+};
+
+VK_DEFINE_HANDLE_CASTS(terakan_pipeline_layout, vk.base, VkPipelineLayout,
+                       VK_OBJECT_TYPE_PIPELINE_LAYOUT);
+
+#endif /* TERAKAN_PIPELINE_LAYOUT_H */
