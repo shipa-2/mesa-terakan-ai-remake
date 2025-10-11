andrewgigena/mesa-terakan

terakan: Depth bias
Signed-off-by: Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>

src/amd/terascale/vulkan/terakan_pipeline_graphics.c
@@ -252,6 +252,43 @@ terakan_pipeline_graphics_apply_pa_su_sc_mode_cntl(
 }
 
 static void
+terakan_pipeline_graphics_apply_pa_su_poly_offset(
+   struct terakan_gfx_command_writer * const command_writer,
+   struct terakan_pipeline_graphics const * const pipeline)
+{
+   /* These values are not needed by internal draws, modify hw_state_draw directly. */
+   bool const clamp_scale_offset_modified =
+      memcmp(&command_writer->hw_state_draw.pa_su_poly_offset_clamp,
+             &pipeline->pre_rasterization.pa_su_poly_offset.clamp, sizeof(float)) != 0 ||
+      memcmp(&command_writer->hw_state_draw.pa_su_poly_offset_subpixel_slope_scale,
+             &pipeline->pre_rasterization.pa_su_poly_offset.subpixel_slope_scale,
+             sizeof(float)) != 0 ||
+      memcmp(&command_writer->hw_state_draw.pa_su_poly_offset_offset,
+             &pipeline->pre_rasterization.pa_su_poly_offset.offset, sizeof(float)) != 0;
+   command_writer->hw_state_draw.pa_su_poly_offset_clamp =
+      pipeline->pre_rasterization.pa_su_poly_offset.clamp;
+   command_writer->hw_state_draw.pa_su_poly_offset_subpixel_slope_scale =
+      pipeline->pre_rasterization.pa_su_poly_offset.subpixel_slope_scale;
+   command_writer->hw_state_draw.pa_su_poly_offset_offset =
+      pipeline->pre_rasterization.pa_su_poly_offset.offset;
+   terakan_hw_state_draw_written(&command_writer->hw_state_draw,
+                                 TERAKAN_HW_STATE_DRAW_INDEX_PA_SU_POLY_OFFSET_CLAMP_SCALE_OFFSET,
+                                 clamp_scale_offset_modified);
+
+   if (command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation !=
+          pipeline->pre_rasterization.pa_su_poly_offset.representation ||
+       command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation_exact !=
+          pipeline->pre_rasterization.pa_su_poly_offset.representation_exact) {
+      command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation =
+         pipeline->pre_rasterization.pa_su_poly_offset.representation;
+      command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation_exact =
+         pipeline->pre_rasterization.pa_su_poly_offset.representation_exact;
+      terakan_state_draw_set_pending(&command_writer->state_draw,
+                                     TERAKAN_STATE_DRAW_INDEX_PA_SU_POLY_OFFSET_DB_FMT_CNTL);
+   }
+}
+
+static void
 terakan_pipeline_graphics_apply_db_render_override_pre_rasterization(
    struct terakan_gfx_command_writer * const command_writer,
    struct terakan_pipeline_graphics const * const pipeline)
@@ -443,6 +480,8 @@ static terakan_pipeline_graphics_apply_state_function const
          terakan_pipeline_graphics_apply_pa_cl_clip_cntl,
       [TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_SC_MODE_CNTL] =
          terakan_pipeline_graphics_apply_pa_su_sc_mode_cntl,
+      [TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_POLY_OFFSET] =
+         terakan_pipeline_graphics_apply_pa_su_poly_offset,
       [TERAKAN_PIPELINE_GRAPHICS_STATE_DB_RENDER_OVERRIDE_PRE_RASTERIZATION] =
          terakan_pipeline_graphics_apply_db_render_override_pre_rasterization,
       [TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SC_AA_MASK] =
@@ -836,6 +875,20 @@ terakan_pipeline_graphics_pre_rasterization_init(
                pipeline->pre_rasterization.pa_su_sc_mode_cntl_clear));
       if (pipeline->pre_rasterization.pa_su_sc_mode_cntl_clear != UINT32_MAX) {
          BITSET_SET(pipeline->static_state, TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_SC_MODE_CNTL);
+      }
+
+      /* TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_POLY_OFFSET */
+      if (!BITSET_TEST(state->dynamic, MESA_VK_DYNAMIC_RS_DEPTH_BIAS_FACTORS)) {
+         pipeline->pre_rasterization.pa_su_poly_offset.clamp = state->rs->depth_bias.clamp;
+         pipeline->pre_rasterization.pa_su_poly_offset.subpixel_slope_scale =
+            TERAKAN_HW_STATE_DRAW_POLY_OFFSET_SLOPE_SUBPIXELS_IN_PIXEL *
+            state->rs->depth_bias.slope;
+         pipeline->pre_rasterization.pa_su_poly_offset.offset = state->rs->depth_bias.constant;
+         pipeline->pre_rasterization.pa_su_poly_offset.representation =
+            state->rs->depth_bias.representation;
+         pipeline->pre_rasterization.pa_su_poly_offset.representation_exact =
+            state->rs->depth_bias.exact;
+         BITSET_SET(pipeline->static_state, TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_POLY_OFFSET);
       }
    }
 }

shipa-2/mesa-terakan-ai-remake

src/amd/terascale/vulkan/terakan_pipeline_graphics.c
@@ -252,6 +252,43 @@ terakan_pipeline_graphics_apply_pa_su_sc_mode_cntl(
 }
 
 static void
+terakan_pipeline_graphics_apply_pa_su_poly_offset(
+   struct terakan_gfx_command_writer * const command_writer,
+   struct terakan_pipeline_graphics const * const pipeline)
+{
+   /* These values are not needed by internal draws, modify hw_state_draw directly. */
+   bool const clamp_scale_offset_modified =
+      memcmp(&command_writer->hw_state_draw.pa_su_poly_offset_clamp,
+             &pipeline->pre_rasterization.pa_su_poly_offset.clamp, sizeof(float)) != 0 ||
+      memcmp(&command_writer->hw_state_draw.pa_su_poly_offset_subpixel_slope_scale,
+             &pipeline->pre_rasterization.pa_su_poly_offset.subpixel_slope_scale,
+             sizeof(float)) != 0 ||
+      memcmp(&command_writer->hw_state_draw.pa_su_poly_offset_offset,
+             &pipeline->pre_rasterization.pa_su_poly_offset.offset, sizeof(float)) != 0;
+   command_writer->hw_state_draw.pa_su_poly_offset_clamp =
+      pipeline->pre_rasterization.pa_su_poly_offset.clamp;
+   command_writer->hw_state_draw.pa_su_poly_offset_subpixel_slope_scale =
+      pipeline->pre_rasterization.pa_su_poly_offset.subpixel_slope_scale;
+   command_writer->hw_state_draw.pa_su_poly_offset_offset =
+      pipeline->pre_rasterization.pa_su_poly_offset.offset;
+   terakan_hw_state_draw_written(&command_writer->hw_state_draw,
+                                 TERAKAN_HW_STATE_DRAW_INDEX_PA_SU_POLY_OFFSET_CLAMP_SCALE_OFFSET,
+                                 clamp_scale_offset_modified);
+
+   if (command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation !=
+          pipeline->pre_rasterization.pa_su_poly_offset.representation ||
+       command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation_exact !=
+          pipeline->pre_rasterization.pa_su_poly_offset.representation_exact) {
+      command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation =
+         pipeline->pre_rasterization.pa_su_poly_offset.representation;
+      command_writer->state_draw.pa_su_poly_offset_db_fmt_cntl.representation_exact =
+         pipeline->pre_rasterization.pa_su_poly_offset.representation_exact;
+      terakan_state_draw_set_pending(&command_writer->state_draw,
+                                     TERAKAN_STATE_DRAW_INDEX_PA_SU_POLY_OFFSET_DB_FMT_CNTL);
+   }
+}
+
+static void
 terakan_pipeline_graphics_apply_db_render_override_pre_rasterization(
    struct terakan_gfx_command_writer * const command_writer,
    struct terakan_pipeline_graphics const * const pipeline)
@@ -443,6 +480,8 @@ static terakan_pipeline_graphics_apply_state_function const
          terakan_pipeline_graphics_apply_pa_cl_clip_cntl,
       [TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_SC_MODE_CNTL] =
          terakan_pipeline_graphics_apply_pa_su_sc_mode_cntl,
+      [TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_POLY_OFFSET] =
+         terakan_pipeline_graphics_apply_pa_su_poly_offset,
       [TERAKAN_PIPELINE_GRAPHICS_STATE_DB_RENDER_OVERRIDE_PRE_RASTERIZATION] =
          terakan_pipeline_graphics_apply_db_render_override_pre_rasterization,
       [TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SC_AA_MASK] =
@@ -836,6 +875,21 @@ terakan_pipeline_graphics_pre_rasterization_init(
                pipeline->pre_rasterization.pa_su_sc_mode_cntl_clear));
       if (pipeline->pre_rasterization.pa_su_sc_mode_cntl_clear != UINT32_MAX) {
          BITSET_SET(pipeline->static_state, TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_SC_MODE_CNTL);
+      }
+
+      /* TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_POLY_OFFSET */
+      if (!BITSET_TEST(state->dynamic, MESA_VK_DYNAMIC_RS_DEPTH_BIAS_FACTORS)) {
+         pipeline->pre_rasterization.pa_su_poly_offset.clamp = state->rs->depth_bias.clamp;
+         pipeline->pre_rasterization.pa_su_poly_offset.subpixel_slope_scale =
+            TERAKAN_HW_STATE_DRAW_POLY_OFFSET_SLOPE_SUBPIXELS_IN_PIXEL *
+            state->rs->depth_bias.slope_factor;
+         pipeline->pre_rasterization.pa_su_poly_offset.offset =
+            state->rs->depth_bias.constant_factor;
+         pipeline->pre_rasterization.pa_su_poly_offset.representation =
+            state->rs->depth_bias.representation;
+         pipeline->pre_rasterization.pa_su_poly_offset.representation_exact =
+            state->rs->depth_bias.exact;
+         BITSET_SET(pipeline->static_state, TERAKAN_PIPELINE_GRAPHICS_STATE_PA_SU_POLY_OFFSET);
       }
    }
 }